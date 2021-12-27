f8741484d5e0d5b2e50f839f3e3bef9622835bdc
Levi Nunnink <levi@culturezoo.com>
2021-12-27 14:48:13 -0500

    Fix build

---
+++ index.js
@@ -0,0 +1,6 @@
+const ParseSiteCommand = require('./lib/ParseSiteCommand');
+const ParseS3SiteCommand = require('./lib/ParseS3SiteCommand');
+
+module.exports = {
+  ParseSiteCommand, ParseS3SiteCommand
+};
